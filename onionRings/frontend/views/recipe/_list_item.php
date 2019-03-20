43bb8b6fe2eb2bf0feb0754b6dc59a5c6f5a9fc2
Mohamad Othman <osmancode@gmail.com>
2019-03-20 18:37:09 +0100

    Added recipe link

--- onionRings/frontend/views/recipe/_list_item.php
+++ onionRings/frontend/views/recipe/_list_item.php
@@ -1,7 +1,17 @@
+<?php
+// _list_item.php
+use yii\helpers\Html;
+use yii\helpers\Url;
+
+?>
 <div class="col-md-4">
     <div class="thumbnail">
-        <img src="<?=Yii::getAlias('@web') . '/' . 'uploads/' . $model->recipe_picture ?>"
-             class="figure-img img-fluid img-rounded"/>
-        <div class="caption"> <?= $model->recipe_title ?>, <?= $model->getCategoryName($model->recipe_category) ?> </div>
+        <a href="<?= Url::toRoute(['recipe/view', 'id' => $model->recipe_id]) ?>">
+            <img src="<?= Yii::getAlias('@web') . '/' . 'uploads/' . $model->recipe_picture ?>"
+                 class="figure-img img-fluid img-rounded"/>
+            <div class="caption"> <?= $model->recipe_title ?>
+                , <?= $model->getCategoryName($model->recipe_category) ?> </div>
+        </a>
     </div>
+
 </div>
